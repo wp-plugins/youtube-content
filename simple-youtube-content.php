4d27aab2f33daea0bb2fe846e237c3fc0a3c5e13
almos20 <almos20@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-21 06:59:32 +0000

    fix version number
    git-svn-id: https://plugins.svn.wordpress.org/youtube-content/trunk@826464 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- simple-youtube-content.php
+++ simple-youtube-content.php
@@ -4,7 +4,7 @@
   Plugin URI: http://www.sandorkovacs.ro/simple-sticky-footer-wordpress-plugin/
   Description: Lightweight Sticky Footer plugin
   Author: Sandor Kovacs
-  Version: 1.2.0
+  Version: 1.0.0
   Author URI: http://sandorkovacs.ro/
  */
 
